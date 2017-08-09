d6763a12643460d79737702e6d81e4ca517fd91f
Aaron Rice <adrice727@gmail.com>
2017-08-09 12:26:16 -0700

    Finish chaper1.

--- chapter1/main.scala
+++ chapter1/main.scala
@@ -1,19 +1,38 @@
+import cats.syntax.show._
+
 
 trait Printable[A] {
   def format(input: A): String
 }
 
-object PrintableInstances = {
+object PrintableInstances {
   implicit val stringPrintable = new Printable[String] {
     def format(input: String): String = input
   }
   implicit val intPrintable = new Printable[Int] {
     def format(input: Int): String = input.toString
   }
+  implicit val catPrintable = new Printable[Cat] {
+    def format(cat: Cat): String = s"${cat.name} is a ${cat.age} year-old ${cat.color} cat"
+  }
+}
+
+object PrintableSyntax {
+  implicit class PrintOps[A](value: A) {
+    def format(implicit p: Printable[A]): String = p.format(value)
+    def print(implicit p: Printable[A]): Unit = println(p.format(value))
+  }
 }
 
 object Printable {
-  implicit def format[A](input: A, implicit p: Printable[A]): String = p.format(input)
-  implicit def print[A](input: A, implicit p: Printable[A]): Unit = println(format(input))
+  def format[A](input: A)(implicit p: Printable[A]): String = p.format(input)
+  def print[A](input: A)(implicit p: Printable[A]): Unit = println(format(input))
 }
 
+final case class Cat(
+  name: String,
+  age: Int,
+  color: String
+)
+
+implicit val catShow: Show[Cat] = Show.show(cat => s"${cat.name} is a ${cat.age} year-old ${cat.color} cat")
